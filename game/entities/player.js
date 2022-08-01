7c2f13bb74dec57508fe5b5b609530ecc4479679
Robert Friedl <robert.friedl@equipmentshare.com>
2022-08-01 13:49:52 -0500

    fix player movement lag

--- game/entities/player.js
+++ game/entities/player.js
@@ -15,38 +15,11 @@ export default class PlayerEntity extends me.Sprite {
       anchorPoint: new me.Vector2d(0.5, 0.5),
      });
 
-    // set a "player object" type
-    // this.body.collisionType = me.collision.types.PLAYER_OBJECT;
-
     // player can exit the viewport (jumping, falling into a hole, etc.)
     this.alwaysUpdate = true;
-    // this.isKinematic = true;
 
-    // this.body.setMaxVelocity(4, 4);
-    // this.body.setFriction(1, 1);
-
-    // set the viewport to follow this renderable on both axis, and enable damping
+    // set the viewport to follow this on both axis, and enable damping
     me.game.viewport.follow(this, me.game.viewport.AXIS.BOTH, 1);
-
-    // enable keyboard
-    // me.input.bindKey(me.input.KEY.LEFT, "left");
-    // me.input.bindKey(me.input.KEY.RIGHT, "right");
-    // me.input.bindKey(me.input.KEY.UP, "up");
-    // me.input.bindKey(me.input.KEY.DOWN, "down");
-
-    // me.input.bindKey(me.input.KEY.A, "left");
-    // me.input.bindKey(me.input.KEY.D, "right");
-    // me.input.bindKey(me.input.KEY.W, "up");
-    // me.input.bindKey(me.input.KEY.S, "down");
-
-    // this.renderable = new me.Sprite(0, 0, {
-    //   image: "Male-01-1",
-    //   framewidth: 32,
-    //   frameheight: 32,
-    //   anchorPoint: new me.Vector2d(0.5, 0.5),
-    // });
-
-    // this.renderable.isKinematic = true;
 
     // define a basic walking animatin
     this.addAnimation("walk-down", [0, 1, 2]);
@@ -60,9 +33,12 @@ export default class PlayerEntity extends me.Sprite {
     this.goToPos = null;
   }
 
-  /**
-   ** update the force applied
-   */
+  setCurrentAnimationIfNotSet(animation){
+    if (!this.isCurrentAnimation(animation)) {
+      this.setCurrentAnimation(animation);
+    }
+  }
+
   update(dt) {
     if(this.navPath.length > 0 && this.goToPos == null){
       this.goToPos = this.navPath.pop();
@@ -71,51 +47,39 @@ export default class PlayerEntity extends me.Sprite {
     if(this.goToPos){
       const vel = new me.Vector2d(0,0);
       if(Math.abs(this.goToPos.x - this.pos.x) > 4){
-        this.pos.x += Math.sign(this.goToPos.x-this.pos.x) * 4;
-      }
-      else {
-        this.pos.x = this.goToPos.x;
+        vel.x = Math.sign(this.goToPos.x-this.pos.x) * 4;
       }
 
       if(Math.abs(this.goToPos.y - this.pos.y) > 4){
-        this.pos.y += Math.sign(this.goToPos.y-this.pos.y) * 4;
-      }
-      else {
-        this.pos.y = this.goToPos.y;
+        vel.y = Math.sign(this.goToPos.y-this.pos.y) * 4;
       }
 
-      if(this.pos.x === this.goToPos.x && this.pos.y === this.goToPos.y){
+      if(!vel.x && !vel.y){
+        this.pos.setV(this.goToPos);
         this.goToPos = null;
       }
-    }
-    // if (me.input.isKeyPressed("up")) {
-    //   this.body.force.y = -this.body.maxVel.y;
-    //   if (!this.renderable.isCurrentAnimation("walk-up")) {
-    //     this.renderable.setCurrentAnimation("walk-up");
-    //   }
-    // } else if (me.input.isKeyPressed("down")) {
-    //   this.body.force.y = this.body.maxVel.y;
-    //   if (!this.renderable.isCurrentAnimation("walk-down")) {
-    //     this.renderable.setCurrentAnimation("walk-down");
-    //   }
-    // } else if (me.input.isKeyPressed("left")) {
-    //   this.body.force.x = -this.body.maxVel.x;
-    //   if (!this.renderable.isCurrentAnimation("walk-left")) {
-    //     this.renderable.setCurrentAnimation("walk-left");
-    //   }
-    // } else if (me.input.isKeyPressed("right")) {
-    //   this.body.force.x = this.body.maxVel.x;
-    //   if (!this.renderable.isCurrentAnimation("walk-right")) {
-    //     this.renderable.setCurrentAnimation("walk-right");
-    //   }
-    // }
+      else {
+        this.pos.add(vel);
 
-    // // check if we moved (an "idle" animation would definitely be cleaner)
-    // if (this.body.vel.x !== 0 || this.body.vel.y !== 0) {
-    //   super.update(dt);
-    //   return true;
-    // }
-    // return false;
+        if(vel.x > 0){
+          this.setCurrentAnimationIfNotSet('walk-right')
+        }
+        else if(vel.x < 0){
+          this.setCurrentAnimationIfNotSet('walk-left')
+        }
+        else if(vel.y > 0){
+          this.setCurrentAnimationIfNotSet('walk-down')
+        }
+        else if(vel.y < 0){
+          this.setCurrentAnimationIfNotSet('walk-up')
+        }
+      }
+
+      super.update(dt);
+      return true;
+    }
+
+    return false;
   }
 
   setNavPath(path){
