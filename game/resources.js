e3697728ede01375a9fc009c394447eaed71c2e3
Robert Friedl <robert.friedl@equipmentshare.com>
2022-08-03 08:04:38 -0500

    remove old tilesets and maps

--- game/resources.js
+++ game/resources.js
@@ -34,48 +34,6 @@ function image(basePath) {
 }
 
 export const hubWorldResources = [
-  // images
-  // { name: "doors", type: "image", src: "tilesets/doors.png" },
-  // { name: "walls", type: "image", src: "tilesets/walls.png" },
-  // { name: "TX Plant", type: "image", src: "tilesets/TX Plant.png" },
-  // { name: "TX Player", type: "image", src: "tilesets/TX Player.png" },
-  // { name: "TX Props", type: "image", src: "tilesets/TX Props.png" },
-  // {
-  //   name: "TX Shadow Plant",
-  //   type: "image",
-  //   src: "tilesets/TX Shadow Plant.png",
-  // },
-  // { name: "TX Shadow", type: "image", src: "tilesets/TX Shadow.png" },
-  // { name: "TX Struct", type: "image", src: "tilesets/TX Struct.png" },
-  // {
-  //   name: "TX Tileset Grass",
-  //   type: "image",
-  //   src: "tilesets/TX Tileset Grass.png",
-  // },
-  // {
-  //   name: "TX Tileset Stone Ground",
-  //   type: "image",
-  //   src: "tilesets/TX Tileset Stone Ground.png",
-  // },
-  // {
-  //   name: "TX Tileset Wall",
-  //   type: "image",
-  //   src: "tilesets/TX Tileset Wall.png",
-  // },
-
-  // maps
-  // { name: "hub", type: "tmx", src: "maps/main.tmx" },
-  // { name: "right", type: "tmx", src: "maps/right.tmx" },
-
-  // tilesets
-  // { name: "doors", type: "tsx", src: "tilesets/doors.tsx" },
-  // { name: "walls", type: "tsx", src: "tilesets/walls.tsx" },
-  // { name: "Tileset Grass", type: "tsx", src: "tilesets/Tileset Grass.tsx" },
-  // { name: "Tileset Wall", type: "tsx", src: "tilesets/Tileset Wall.tsx" },
-  // { name: "Struct", type: "tsx", src: "tilesets/Struct.tsx" },
-  // { name: "Shadow", type: "tsx", src: "tilesets/Shadow.tsx" },
-  // { name: "Props", type: "tsx", src: "tilesets/Props.tsx" },
-  // { name: "Plant", type: "tsx", src: "tilesets/Plant.tsx" },
   ...map("maps/iceland-test"),
   ...map("maps/minifantasy-dungeon"),
 
@@ -89,6 +47,8 @@ export const hubWorldResources = [
   ...tileset(
     "tilesets/Minifantasy_Dungeon/Minifantasy_DungeonWallTilesetShadows"
   ),
+  ...tileset('tilesets/Minifantasy_IcyWilderness/Minifantasy_IcyWildernessAllTiles'),
+  ...tileset('tilesets/Minifantasy_IcyWilderness/Minifantasy_IcyWildernessAllTilesShadows')
 ];
 
 export const globalResources = [
--- game/tilesets/doors.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.0" name="doors" tilewidth="32" tileheight="32" tilecount="25" columns="5">
- <image source="doors.png" width="160" height="160"/>
-</tileset>
